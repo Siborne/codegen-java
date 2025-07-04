ca003dd462cdeaf839611008e00d4a89419e1605
ubanXiaOvo <904389425@qq.com>
2025-07-05 02:38:03 +0800

    feat: 添加代码生成工具和示例项目
    - 新增 codegen-java 项目，用于生成代码- 新增 codegen-java-demo项目，作为代码生成的示例
    - 添加数据库配置文件和工具类
    - 配置 Maven依赖和构建插件

---
+++ codegen-java/src/main/java/org/flyingsparrow/RunApplication.java
@@ -0,0 +1,9 @@
+package org.flyingsparrow;
+
+import org.flyingsparrow.builder.BuildTable;
+
+public class RunApplication {
+    public static void main(String[] args) {
+        BuildTable.geetTables();
+    }
+}
