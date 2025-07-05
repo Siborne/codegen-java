139c2ca27c1392b75865d539d43ccaf8d8b8f204
ubanXiaOvo <904389425@qq.com>
2025-07-05 17:54:36 +0800

    feat: 更新数据库配置并添加常量定义
    - 修改数据库连接 URL 为 codegen_java
    - 添加是否忽略表前缀的配置- 更新参数 bean 后缀为 Query
    - 在 Constants 类中添加 SQL 数据类型常量

--- codegen-java/src/main/java/org/flyingsparrow/bean/Constants.java
+++ codegen-java/src/main/java/org/flyingsparrow/bean/Constants.java
@@ -16,4 +16,18 @@ public class Constants {
         IGNORE_TABLE_PERFIX = Boolean.valueOf(PropertiesUtils.getProperty("ignore.table.prefix"));
         SUFFIX_BEAN_PARAM = PropertiesUtils.getProperty("suffix.bean.param");
     }
+
+    public final static String[] SQL_DATE_TIME_TYPES = new String[]{"datetime", "timestamp"};
+
+    public final static String[] SQL_DATE_TYPES = new String[]{"date"};
+
+    public static final  String[] SQL_DECIMAL_TYPE = new String[]{"decimal", "double","float"};
+
+    public static final String[] SQL_STRING_TYPE = new String[]{"char", "varchar", "text","mediumtext","longtext"};
+
+    //Integer
+    public static final String[] SQL_INTEGER_TYPE = new String[]{"int","tinyint"};
+
+    //Long
+    public static final String[] SQL_LONG_TYPE = new String[]{"bigint"};
 }
